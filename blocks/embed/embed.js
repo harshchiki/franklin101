fba73ff03da7e080bd33eb3507578423ebb24875
Harsh Chiki <chiki@adobe.com>
2024-03-12 14:28:57 +0530

    video player changes

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -16,7 +16,7 @@ const loadScript = (url, callback, type) => {
   return script;
 };
 
-const getDefaultEmbed = (url) => `<div abcd style="left: 0; width: 600px; height: 300px; position: relative; ">
+const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
         <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
           scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
         </iframe>
